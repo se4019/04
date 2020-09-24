45261940f9017bf417e99c6e23506249fe21c875
se4019 <70657061+se4019@users.noreply.github.com>
2020-09-24 16:40:15 +0900

    04_04

---
+++ main4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int input;
+	int sec, min;
+	
+	printf("input year : ");
+	
+	//scanf
+	scanf("%i", &input);
+	
+	
+	//print
+	printf("is %i leap yaer? %i/n", input,
+									(input%4==0 && input%100!=0)||(input%400==0) );
+	
+	
+	return 0;
+}
