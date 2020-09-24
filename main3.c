413e4380787aa71b595135b451f6efe5c2e0bad8
se4019 <70657061+se4019@users.noreply.github.com>
2020-09-24 16:25:39 +0900

    04_03

---
+++ main3.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	int input;
+	int sec, min;
+	
+	printf("input second : ");
+	
+	//scanf
+	scanf("%i", &input);
+	
+	//calculate min
+	min = input / 60;
+	
+	//calculate sec
+	sec = input % 60;
+	
+	//print
+	printf("the time is %i : %i/n", min, sec);
+	
+	return 0;
+}
